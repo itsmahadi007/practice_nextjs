4feef6c3532044362c0f164c95ae3934b786b068
Mahadi Hassan <itsmahadi@gmail.com>
2024-10-17 21:52:00 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import ProductCard from "@/app/components/ProductCard/ProductCard";
+import React from "react";
 
 export default function Home() {
     return (
         <main>
-            <h1>Hello World!</h1>
+            <div className="text-3xl text-sky-800">Hello World!</div>
             <Link href="/users"> Users</Link>
+            <ProductCard/>
         </main>
 
     );
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
-const UsersPage = () => {
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users',
+        {cache: 'no-store'}
+    );
+    const users: User[] = await res.json();
     return (
-        <div>
-            Users Page
-        </div>
+        <>
+            <h1 className="text-3xl">Users</h1>
+            {/*<p>{new Date().toLocaleTimeString()}</p>*/}
+            <br/>
+            <table className="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    users.map(
+                        user =>
+                            <tr key={user.id}>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                            </tr>
+                    )
+                }
+                </tbody>
+            </table>
+        </>
     );
 }
 
